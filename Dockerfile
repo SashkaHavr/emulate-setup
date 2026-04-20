1f83fd64f88ada65b109b441387d1e56893b2001
Oleksandr Havrylov <47502748+SashkaHavr@users.noreply.github.com>
2026-04-20 23:59:01 +0200

    Patch emulate

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM oven/bun:1.3.11-alpine
 WORKDIR /app
 COPY . .
 RUN bun ci --production
-CMD [ "bunx", "emulate", "--service", "google" ]
+CMD [ "bun", "index.ts" ]
---
+++ index.ts
@@ -0,0 +1,33 @@
+import { createEmulator } from "emulate";
+
+console.log("Starting emulator...");
+
+const secret = process.env.EMULATE_SECRET ?? "GOCSPX-secret";
+const uris: string[] = process.env.EMULATE_URIS ? JSON.parse(process.env.EMULATE_URIS) : [];
+
+const users = Array.from({ length: 20 }).map((_, i) => ({
+  email: `user${i}@example.com`,
+  name: `Test User ${i}`,
+}));
+
+const google = await createEmulator({
+  service: "google",
+  port: 4000,
+  seed: {
+    google: {
+      users,
+      oauth_clients: [
+        {
+          client_id: "my-client-id.apps.googleusercontent.com",
+          client_secret: secret,
+          redirect_uris: ["localhost", "127.0.0.1", ...uris],
+        },
+      ],
+    },
+  },
+});
+
+process.on("SIGINT", async () => {
+  console.log("Closing emulator...");
+  await google.close();
+});
